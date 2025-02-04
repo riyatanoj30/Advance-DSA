db6889868a9b36bd680802cfffc17f57ce18bb83
Riya Mhatre <riyatmhatre@gmail.com>
2025-02-04 11:20:22 +0530

    First commit of StringConcatExample

---
+++ StringConcatExample.java
@@ -0,0 +1,16 @@
+public class StringConcatExample {
+    public static void main(String[] args) {
+        String str1 = "sunstone";
+        String str2 = "ADYPU";
+        String result = str1.concat(str2);
+        System.out.println("Concatenated String: " + result);
+    }
+}
+
+// public class StringConcatExample {
+//     public static void main(String[] args) {
+//         String str1 = "sunstone";
+//         String str2 = "ADYPU";
+//         System.out.println("Concatenated String: " + str1.concat(str2));
+//     }
+// }
