d92a5409745dbf7776024edf1f6deafc38572acb
Riya Mhatre <riyatmhatre@gmail.com>
2025-02-04 11:15:16 +0530

    First commit of StringLengthExample

---
+++ StringLengthExample.java
@@ -0,0 +1,14 @@
+public class StringLengthExample {
+    public static void main(String[] args) {
+        String str = "sunstone";
+        int length = str.length();
+        System.out.println("The length of the word 'sunstone' is: " + length);
+    }
+}
+
+// public class StringLengthExample {
+//     public static void main(String[] args) {
+//         String str = "sunstone";
+//         System.out.println("The length of the word 'sunstone' is: " + str.length());
+//     }
+// }
